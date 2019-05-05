53b0ffa512c75afe7cbb54e960b33cfd9d6a3217
fallen.ink <fengzilijie@qq.com>
2019-05-05 11:42:39 +0800

    文件大改

--- _Building/Base/BaseNavigationController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <_Foundation/_Foundation.h>
-
-@interface BaseNavigationController : UINavigationController
-
-@prop_assign( BOOL,         autoHideBottomBar )         // [YES]
-
-@end
